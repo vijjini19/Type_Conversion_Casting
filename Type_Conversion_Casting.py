11334fefe6a8acca90ab8b8f10a31b55565bf5e8
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-06-17 17:31:48 +0530

    Add files via upload

---
+++ Type_Conversion_Casting.py
@@ -0,0 +1,42 @@
+# a=3
+# print(type(a))
+# print(a,type(a))
+# print(float(a)) #3.0
+# print(str(a)) #3
+
+# b=7.2
+# print(type(b))
+# print(b,type(b))
+# print(int(b)) #7
+# print(str(b)) #7.2
+
+# c='python'
+# print(type(c))
+# print(c,type(c))
+# print(int(c)) #error
+# print(float(c)) #error
+
+# x='76'
+# print(type(x))
+# print(x,type(x))
+# print(int(x)) #76
+# print(float(x)) #76.0
+
+# y='6.3'
+# print(type(y))
+# print(y,type(y))
+# print(int(y)) #error
+# print(float(y)) #6.3
+# z=float(y) #z=6.3
+# print(int(z)) #6
+
+
+
+
+
+
+
+
+
+
+
